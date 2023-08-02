6537b09a8192b4312bdac28efd0b3ce67d257d36
b1806a0 <38452681+b1806a0@users.noreply.github.com>
2023-08-02 14:18:52 +0900

    Update all2021.py

--- apps/all2021.py
+++ apps/all2021.py
@@ -1,13 +1,6 @@
 import os
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 import streamlit as st
 
-//日本語文字化け対策でフォント指定
-sns.set(font='Hiragino Sans')
-plt.rcParams['font.family'] = "Hiragino Sans"
 
 def app():
 

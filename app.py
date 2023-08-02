a893bff694fa393cda23491ede8436eca8d87bf5
b1806a0 <38452681+b1806a0@users.noreply.github.com>
2023-08-02 16:22:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import streamlit as st
 from multiapp import MultiApp
 from apps import(
-class2021,
-all2021,
+classes,
+all,
 )
 
 apps = MultiApp()
 
 # Add all your application here
-apps.add_app("結果", class2021.app)
-apps.add_app("分析", all2021.app)
+apps.add_app("結果", classes.app)
+apps.add_app("分析", all.app)
 
 # The main app
 apps.run()

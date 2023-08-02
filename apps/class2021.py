40763e46ee0adf47eb16dd8859e72d61fe6f2277
b1806a0 <38452681+b1806a0@users.noreply.github.com>
2023-08-02 14:20:15 +0900

    Update class2021.py

--- apps/class2021.py
+++ apps/class2021.py
@@ -1,10 +1,10 @@
 import os
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+#import pandas as pd
+#import numpy as np
+#import matplotlib.pyplot as plt
+#import seaborn as sns
 import streamlit as st
-import japanize_matplotlib
+#import japanize_matplotlib
 
 def app():
 
